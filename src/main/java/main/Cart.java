8de3d3da0f18528a94b7a76170f3d191e5c877fa
Chaitanya Gupta <Chaitanya Gupta@DESKTOP-OVFVDB3>
2019-07-25 01:45:23 +0530

    Hai

--- src/main/java/main/Cart.java
+++ src/main/java/main/Cart.java
@@ -9,6 +9,7 @@ public class Cart extends Bill{
     //one cart one bill
     public List<Item> inCart = new ArrayList<Item>();
     public List<Integer> quant = new ArrayList<Integer>();
+   // public int noOfItems=0; //replaced by next line
     public int noOfItems=0;
     public void updateCart(Item a,int q ){
       //write your code here
@@ -16,7 +17,7 @@ public class Cart extends Bill{
       //------------
     	inCart.add(a);
     	quant.add(q);
-   
+    	noOfItems = noOfItems +1;
 
     }
     public int totalAmt=0;
@@ -26,12 +27,17 @@ public class Cart extends Bill{
             System.out.println("|  "+ inCart.get(i).itemName + "  " + inCart.get(i).mrp +"  "+quant.get(i)+'\n');
         }
         totalAmt = calcBill();
+       // System.out.println("no of items entered " + noOfItems);
         System.out.println("total Amount: "+ Integer.toString(totalAmt));
     }
     public int calcBill() {
     	int total=0;
 //    	write your code here!!!
 //    	----------------------
+    	for (int i=0; i< noOfItems; i++) {
+    		
+    		total=total+((quant.get(i)) * (inCart.get(i).mrp));
+    	}
     	return total;
     	
     }
